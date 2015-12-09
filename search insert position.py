cfc004458d95da8b0a1ea1a540906e626ee8ebaf
yun1989yue <418292922@qq.com>
2015-12-09 13:00:13 -0800

    Update search insert position.py

--- search insert position.py
+++ search insert position.py
@@ -44,3 +44,23 @@ class Solution(object):
                     return end
                 end = mid - 1
         return start
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) / 2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] == target:
+                start = mid
+                break
+            else: # if nums[end] < target, end + 1 will be returned
+                end = mid - 1
+        return start
