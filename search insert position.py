378be254ab0ff44ec5a111a0adac12d5b9762f2e
yun1989yue <418292922@qq.com>
2015-12-09 12:57:11 -0800

    Create search insert position.py

---
+++ search insert position.py
@@ -0,0 +1,46 @@
+'''
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+'''
+'''
+Brute Force O(n) time O(1) space
+'''
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        res = 0
+        while res < len(nums) and nums[res] < target:
+            res += 1
+        return res
+'''
+binary search O(logn) tiem O(1) space
+'''
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        while start <= end: # find the 1st element >= target, if not, insert at len(nums)
+            mid = (start + end) / 2
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                if end == 0 or nums[end - 1] < target: # check whether it is the 1st element >= target
+                    return end
+                end = mid - 1
+        return start
