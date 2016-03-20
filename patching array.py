73c86d20579c7de85836e1896d173aaf5e295243
yun1989yue <418292922@qq.com>
2016-03-20 17:03:54 +0800

    Update patching array.py

--- patching array.py
+++ patching array.py
@@ -54,3 +54,22 @@ class Solution(object):
                     count += 1
                     far += far + 1
         return count
+
+'''
+Another similar way to explain:
+Let miss be the smallest sum in [0,n] that we might be missing. Meaning we already know we can build all sums in [0,miss). Then if we 
+have a number num <= miss in the given array, we can add it to those smaller sums to build all sums in [0,miss+num). If we don't, then 
+we must add such a number to the array, and it's best to add miss itself, to maximize the reach.
+'''
+int minPatches(vector<int>& nums, int n) {
+    long miss = 1, added = 0, i = 0;
+    while (miss <= n) {
+        if (i < nums.size() && nums[i] <= miss) {
+            miss += nums[i++];
+        } else {
+            miss += miss;
+            added++;
+        }
+    }
+    return added;
+}
