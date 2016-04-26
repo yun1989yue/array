cf507fd1e57d5f51fb1800213f059414df1aca6f
yun1989yue <418292922@qq.com>
2016-04-26 22:24:54 +0800

    Create range_sum_query_2D.py

---
+++ range_sum_query_2D.py
@@ -0,0 +1,61 @@
+'''
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+'''
+class NumMatrix(object):
+    def __init__(self, matrix):
+        """
+        initialize your data structure here.
+        :type matrix: List[List[int]]
+        """
+        self.matrix = matrix
+        for i in xrange(len(self.matrix)): # self.matrix[i][j] = sum(matrix[m][n]) where m <= i and n <= j, i.e. square sum from M00 to Mij
+            for j in xrange(1, len(self.matrix[0])):
+                self.matrix[i][j] += self.matrix[i][j-1]
+        for i in xrange(1, len(self.matrix)):
+            for j in xrange(len(self.matrix[0])):
+                self.matrix[i][j] += self.matrix[i-1][j]
+
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        if row1 == 0 and col1 == 0:
+            return self.matrix[row2][col2]
+        elif row1 == 0:
+            return self.matrix[row2][col2] - self.matrix[row2][col1-1]
+        elif col1 == 0:
+            return self.matrix[row2][col2] - self.matrix[row1-1][col2]
+        else:
+            return self.matrix[row2][col2] - self.matrix[row1-1][col2] - self.matrix[row2][col1-1] + self.matrix[row1-1][col1-1]
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# numMatrix = NumMatrix(matrix)
+# numMatrix.sumRegion(0, 1, 2, 3)
+# numMatrix.sumRegion(1, 2, 3, 4)
