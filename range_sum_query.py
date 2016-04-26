a05a9f164d2bd8b5266c1bd62ba414c0575aa704
yun1989yue <418292922@qq.com>
2016-04-26 16:17:19 +0800

    Create range_sum_query.py

---
+++ range_sum_query.py
@@ -0,0 +1,43 @@
+'''
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+'''
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        # basic idea: accumulation
+        # O(n) time for initialization
+        self.nums = nums
+        for i in xrange(1, len(self.nums)):
+            self.nums[i] += self.nums[i-1]
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        # O(1) time for look
+        if i == 0:
+            return self.nums[j]
+        else:
+            return self.nums[j]-self.nums[i-1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
