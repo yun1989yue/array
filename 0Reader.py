e918808e3d48b90197dbbc6ed72cf6feaf9ed663
yun1989yue <418292922@qq.com>
2016-05-05 12:25:56 +0800

    Create 0Reader.py

---
+++ 0Reader.py
@@ -0,0 +1,31 @@
+&: and operation
+1 & 0 = 0
+0 & 1 = 0
+0 & 0 = 0
+1 & 1 = 1
+|: or operation
+1 | 0 = 1
+0 | 1 = 1
+0 | 0 = 0
+1 | 1 = 1
+^: xor
+1 ^ 0 = 1
+0 ^ 1 = 1
+0 ^ 0 = 0
+1 ^ 1 = 0
+x ^ 0 = x
+x ^ x = 0
+x ^ y ^ x = y
+~: not
+<<: left shift *2
+>>: right shift /2
+
+A. Integer
+    1. Method：
+        1. Dictionary
+        2. sort 
+        3. two pointers
+    2. Notice:
+        1. sorted or not
+        2. repeat or not
+        3. null or not
